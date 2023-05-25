8fc59ce92352aa8d8cf35c27f37df51fac40c0d3
Mariana <>
2023-05-25 10:36:46 -0600

    Proyecto final del parcial

---
+++ src/pr2vmm4s21/PR2VMM4S21.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pr2vmm4s21;
+
+/**
+ *
+ * @author shack
+ */
+public class PR2VMM4S21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new jfprincipal().setVisible(true);
+    }
+    
+}
